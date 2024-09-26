777a9c9c6ace74299b612f460a1d52628d74a57d
apridika <apridika15@gmail.com>
2024-09-26 16:45:40 +0700

    tugas

---
+++ diskon.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main (){
+  int harga = 150000;
+  double diskon = 12.5;
+  double hasildiskon = harga * diskon/100;
+  double hasil = harga - hasildiskon;
+  cout << "Harga : " <<harga << " ribu"<<endl;
+  cout << "Diskon : " <<diskon <<endl;
+  cout << "Besarnya diskon dari harga : " <<hasildiskon <<endl;
+  cout << "Harga setelah diskon " <<hasil <<" ribu";
+  return 0;
+}
